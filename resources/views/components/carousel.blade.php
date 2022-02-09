e33b4f1c1d64d9334879147eddff8f7cca97b829
IAGO NEVES <iago.neves-ms@hotmail.com>
2022-02-09 16:52:08 -0300

    ICONS

--- resources/views/components/carousel.blade.php
+++ resources/views/components/carousel.blade.php
@@ -43,11 +43,11 @@
         </div>
 
         <a class="carousel-control-prev" href="#heroCarousel" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon icofont-rounded-left" aria-hidden="true"></span>
+          <span class="fa-solid fa-angles-left" aria-hidden="true"></span>
           <span class="sr-only">Previous</span>
         </a>
         <a class="carousel-control-next" href="#heroCarousel" role="button" data-slide="next">
-          <span class="carousel-control-next-icon icofont-rounded-right" aria-hidden="true"></span>
+          <span class="fa-solid fa-angles-right" aria-hidden="true"></span>
           <span class="sr-only">Next</span>
         </a>
 
